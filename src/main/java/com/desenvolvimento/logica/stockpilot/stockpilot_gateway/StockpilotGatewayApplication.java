84910ed87100caca6aa9994095abf26caeb09905
rhuan <rhuan.resende@hotmail.com.br>
2026-08-18 02:28:45 -0300

    feat: First commit

---
+++ src/main/java/com/desenvolvimento/logica/stockpilot/stockpilot_gateway/StockpilotGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.logica.stockpilot.stockpilot_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpilotGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpilotGatewayApplication.class, args);
+	}
+
+}
